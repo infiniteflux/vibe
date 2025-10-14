9f3228b4d147041a4bedeaaf4840fd210643c9d0
infiniteflux <Vabhinav12112003@gmail.com>
2025-10-14 15:28:12 +0530

    update

--- app/src/main/java/com/infiniteflux/login_using_firebase/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/infiniteflux/login_using_firebase/screens/profile/ProfileScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.infiniteflux.login_using_firebase.AppRoutes
